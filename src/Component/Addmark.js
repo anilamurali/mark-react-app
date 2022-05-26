b865288e9bca4d0cb08fa09b9567b8099ed34d55
anilamurali <anilamurali98@gmail.com>
2022-05-26 11:33:29 +0530

    first commit

--- src/Component/Addmark.js
+++ src/Component/Addmark.js
@@ -1,24 +1,41 @@
-<div class="container">
+import React, { useState } from 'react'
+
+const Addmark = () => {
+    var [name,setName]=useState("")
+    var [admno,setAdmno]=useState("")
+    var [cgpa,setCgpa]=useState("")
+    const setData=()=>{
+        var data={"name":name,"admno":admno,"cgpa":cgpa}
+        console.log(data)
+    }
+
+  return (
+    <div>
+        <div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div class="row">
+            <div class="row g-3">
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Name</label>
-                    <input type="text" class="form-control">
+                    <input onChange={(e)=>{setName(e.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">Addmission Number</label>
-                    <input type="text" class="form-control">
+                    <input onChange={(e)=>{setAdmno(e.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" class="form-label">CGPA</label>
-                    <input type="text" class="form-control">
+                    <input onChange={(e)=>{setCgpa(e.target.value)}} type="text" class="form-control"/>
                 </div>
                 
                 <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button class="btn btn-dark">SUBMIT</button>
+                    <button onClick={setData} class="btn btn-primary">SUBMIT</button>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</div></div>
+  )
+}
+
+export default Addmark
