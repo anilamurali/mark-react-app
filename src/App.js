209f060fa11d68f6ad448b7c296745d986d874db
anilamurali <anilamurali98@gmail.com>
2022-05-26 11:47:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addmark from './Component/Addmark';
+import Viewmark from './Component/Viewmark';
 
 function App() {
   return (
     <>
-    <Addmark/>
+    <Viewmark/>
     </>
   );
 }
---
+++ src/Component/Viewmark.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Viewmark = () => {
+    var viewmark=[
+        {
+            "name":"anila",
+            "admno":"MZC19MCA004",
+            "cgpa":"7.9"
+        }
+    ]
+  return (
+    <div>
+        <div>
+        
+        <div class="container">
+    <div class="row ">
+        <div class="col-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <table class="table table-striped table-dark">
+                <thead>
+                  <tr>
+                    <th scope="col">NAME</th>
+                    <th scope="col">ADMISSION NUMBER</th>
+                    <th scope="col">CGPA</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {
+                        viewmark.map((value,key)=>{
+                       return <tr>
+                             <td>{value.name}</td>
+                             <td>{value.admno}</td>
+                             <td>{value.cgpa}</td>
+                            
+                             
+                        </tr>
+                        })}
+                </tbody>
+              </table>
+        </div>
+    </div>
+</div>
+    </div>
+    </div>
+  )
+}
+
+export default Viewmark
